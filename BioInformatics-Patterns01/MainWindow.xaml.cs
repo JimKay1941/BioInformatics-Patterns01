0c2fcae10a24575bfb03e4e4d418e3359609e21d
Jam Kay <jam@jimkay.us>
2018-05-11 14:23:26 +0800

    First working version with both conversions and full validity checking.

--- BioInformatics-Patterns01/MainWindow.xaml.cs
+++ BioInformatics-Patterns01/MainWindow.xaml.cs
@@ -20,9 +20,134 @@ namespace BioInformatics_Patterns01
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int PatternLength;
+        public int DecimalIndex;
+        public int Decimal;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_DecimalToDna(object sender, RoutedEventArgs e)
+        {
+            txtStatus.Text = "";
+
+            if (txtLength.Text == "")
+            {
+                txtStatus.Text = @"Length invalid";
+                return;
+            }
+
+            if (!Int32.TryParse(txtLength.Text, out PatternLength))
+            {
+                txtStatus.Text = @"Length invalid";
+                return;
+            }
+
+            if (PatternLength < 1)
+            {
+                txtStatus.Text = @"Length invalid";
+                return;
+            }
+
+            if (txtDecimal.Text == "")
+            {
+                txtStatus.Text = @"Decimal is invalid";
+                return;
+            }
+
+            if (!Int32.TryParse(txtDecimal.Text, out DecimalIndex))
+            {
+                txtStatus.Text = @"Decimal is invalid";
+                return;
+            }
+
+            if (DecimalIndex < 1)
+            {
+                txtStatus.Text = @"Decimal is invalid";
+                return;
+            }
+            
+            if ((Math.Pow(4, PatternLength)) <= DecimalIndex)
+            {
+                txtStatus.Text = @"Decimal or Length invalid";
+                return;
+            }
+
+            string result = "";
+            for (int i = 0; i < PatternLength; i++)
+            {
+                var lowEnd = DecimalIndex % 4;
+                switch (lowEnd)
+                {
+                    case 0:
+                        result = "A" + result;
+                        break;
+                    case 1:
+                        result = "C" + result;
+                        break;
+                    case 2:
+                        result = "G" + result;
+                        break;
+                    case 3:
+                        result = "T" + result;
+                        break;
+                }
+
+                DecimalIndex = DecimalIndex / 4;
+            }
+
+            txtDNA.Text = result;
+        }
+
+        private void Button_DnaToDecimal(object sender, RoutedEventArgs e)
+        {
+            if (txtDNA.Text == "")
+            {
+                txtStatus.Text = @"DNA invalid";
+                return;
+            }
+
+            txtStatus.Text = "";
+            txtDecimal.Text = "";
+
+            txtLength.Text = txtDNA.Text.Length.ToString();
+
+            Decimal = 0;
+            int j = 0;
+            for (int i = txtDNA.Text.Length - 1; i >= 0; i--)
+            {
+                var piece = txtDNA.Text.Substring(i, 1);
+
+                switch (piece)
+                {
+                    case "A":
+                        Decimal += (int) Math.Pow(4, j) * 0;
+                        break;
+                    case "C":
+                        Decimal += (int) Math.Pow(4, j) * 1;
+                        break;
+                    case "G":
+                        Decimal += (int) Math.Pow(4, j) * 2;
+                        break;
+                    case "T":
+                        Decimal += (int) Math.Pow(4, j) * 3;
+                        break;
+                    default:
+                        txtStatus.Text = @"DNA Invalid";
+                        return;
+                }
+
+                j++;
+            }
+
+            txtDecimal.Text = Decimal.ToString();
         }
     }
 }
